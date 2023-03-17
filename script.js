b8a20579290243887d4b98f11ee481448cd198c8
pdasilva11 <50679298+pdasilva11@users.noreply.github.com>
2023-03-16 23:21:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-JavaScript
+<script>
 $(document).ready(function(){
     let apiKey = "##yourapikey##"
  
@@ -12,6 +12,17 @@ $(document).ready(function(){
 function videoSearch(apiKey,search,maxResults){
     $.get("https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,(data) => {
         console.log(data)
+ 
+        let video = ''
+ 
+        data.items.forEach(item => {
+            video = `
+            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            `
+ 
+            $("#videos").append(video)
+        });
     })
  
 }
+</script>
